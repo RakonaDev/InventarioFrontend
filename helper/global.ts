75d619ff01005685cd752f9e51658b7dfc044e7f
JuanDev <juancajas1905@gmail.com>
2025-02-04 10:35:50 -0500

    d

--- helper/global.ts
+++ helper/global.ts
@@ -12,7 +12,14 @@ import axios from "axios"
 export const apiAuth = axios.create({
   baseURL: apiURL,
   withCredentials: true,
-  headers: {
-    Authorization: `Bearer ${localStorage.getItem("token")}`
-  }
 })
+
+apiAuth.interceptors.request.use((config) => {
+  if (typeof window !== "undefined") {
+    const token = localStorage.getItem("token");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+  }
+  return config;
+});
