30095ed69d6ffdb8a858e19af7ea1aaa91daa41b
RakonaDev <juancajas1905@gmail.com>
2025-03-22 10:46:20 -0500

    95% proyecto

--- store/ProductStore.ts
+++
@@ -1,9 +0,0 @@
-import { create } from 'zustand'
-import { InsumoPaginate, InsumoPaginateActions } from "@/interfaces/InsumosInterface";
-
-export const useProductStore = create<InsumoPaginate & InsumoPaginateActions>((set) => {
-  currentPage: 1,
-  setInsumoPaginate: (currentPage: number) => {
-    set({ currentPage })
-  }
-})
